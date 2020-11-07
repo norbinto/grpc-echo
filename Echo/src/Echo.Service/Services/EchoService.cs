3204ad6f09750e4c244fab2a38e31e32084a12da
Anton Sizikov <sizikov.anton@gmail.com>
2020-11-07 21:51:16 +0100

    create new project

---
+++ Echo/src/Echo.Service/Services/EchoService.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Core;
+using System.Threading.Tasks;
+
+namespace Echo.Service.Services
+{
+    public class EchoService: Echoer.EchoerBase
+    {
+        public override Task<EchoReply> Say(SayRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new EchoReply
+            {
+                Message = $"you said: {request.Message}"
+            });
+        }
+    }
+}
