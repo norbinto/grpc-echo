9c1e0be038b42d1bb3f73532ca153d435ae34248
Anton Sizikov <sizikov.anton@gmail.com>
2020-11-08 21:28:01 +0100

    small cleanup

--- Echo/src/Echo.Client/Program.cs
+++ Echo/src/Echo.Client/Program.cs
@@ -5,28 +5,33 @@ using Grpc.Net.Client;
 
 namespace Echo.Client
 {
-    class Program
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class Program
     {
         static async Task Main(string[] args)
         {
             Console.WriteLine("Say something");
             var userInput = Console.ReadLine();
-            var httpHandler = new HttpClientHandler();
-            // Return `true` to allow certificates that are untrusted/invalid
-            httpHandler.ServerCertificateCustomValidationCallback =
-                HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
             // The port number(5001) must match the port of the gRPC server.
-            using var channel = GrpcChannel.ForAddress("https://localhost:5001", new GrpcChannelOptions
-            {
-                HttpHandler = httpHandler
-            });
-            
+            using var channel = GetGrpcChannel();
             var client =  new Echoer.EchoerClient(channel);
             var reply = await client.SayAsync(
                 new SayRequest { Message = userInput });
-            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Received reply: " + reply.Message);
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
+        }
+
+        private static GrpcChannel GetGrpcChannel()
+        {
+            return GrpcChannel.ForAddress("https://localhost:5001", new GrpcChannelOptions
+            {
+                HttpHandler = new HttpClientHandler
+                {
+                    ServerCertificateCustomValidationCallback =
+                        HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+                }
+            });
         }
     }
 }
